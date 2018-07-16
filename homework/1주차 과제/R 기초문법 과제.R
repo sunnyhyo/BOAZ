8ba8ef323001af299393e9f01fa80adfc6782cc7
sunnyhyo <sunnyhyo77@gmail.com>
2018-07-16 21:59:39 +0900

    0716

--- homework/1주차 과제/R 기초문법 과제.R
+++ homework/1주차 과제/R 기초문법 과제.R
@@ -1,10 +1,37 @@
 ###과제
+getwd()
+sample<-read.delim("./sample.txt", header=TRUE)
+head(sample)
+sample
+length(c(1,2,3))
+nrow(sample)
+class(c(1,2,3))
+class(sample)
+dim(sample)
 
 # 1. sum() 함수와 똑같은 기능을 하는 함수를 for문을 이용하여 작성하시오.
 
-
-
-
+sum(1,2,3)
+sum(sample)
+a<-c(1,2,3)
+a[1]
+a[2]
+fuc_sum = function(x){
+  if(class(x)=="numeric"){
+    y=0
+    for (i in 1:length(x)){
+      y= y+x[i]
+    }
+    return(y)
+  }
+  if(class(x)=="data.frame"{
+    y=0
+    for(i in 1:dim(x)[1]){
+      
+    }
+  }  
+  }
+fuc_sum(c(1,2,3))
 
 # 2. which.max() 함수와 똑같은 기능을 하는 함수를 for문, if문을 이용하여 작성하시오.
 
